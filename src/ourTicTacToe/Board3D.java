87a44b76d9ed9aa82052c8645a27e0af7d386e5e
Brian <brianru@live.unc.edu>
2019-11-05 18:42:21 -0500

    Added several methods to board class

--- src/ourTicTacToe/Board3D.java
+++ src/ourTicTacToe/Board3D.java
@@ -1,11 +1,53 @@
 package ourTicTacToe;
 
 public class Board3D {
-	int i=0;
+	private Point[][][] board = new Point[4][4][4];
 	public Board3D(){
 		
+		//populate board with values
+		for (int i=0; i<4; i++) {
+			for (int j=0; j<4; j++) {
+				for (int k=0; k<4; k++) {
+					board[i][j][k] = new Point();
+	
+				}
+			}
+		}
+			
 	}
-	String hiString = "hello";
-	String hiString2 = "hello2";
-	//thing
+	
+	//update board is passed a boolean indicating which player made the move and three ints giving position of changed point
+	public void updateBoard(boolean isP1, int x, int y, int z) {  
+		if (board[x][y][z].value == 0) {   //if the square is blank
+			
+		}
+		//board[x][y][z].
+
+	}
+	
+	public Point[][][] getBoard() {
+		return this.board;
+	}
+	
+	public Point[] getXes() {
+		
+	}
+	
+	public Point[] getYs() {
+		
+	}
+	
+	
+	public void wipeBoard() {
+		for (int i=0; i<4; i++) {
+			for (int j=0; j<4; j++) {
+				for (int k=0; k<4; k++) {
+					board[i][j][k].value = 0; //adjust so that points property of currentvalue is correct
+	
+				}
+			}
+		}
+	}
+	
+
 }
