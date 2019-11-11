0327852211f876281f2dc30f2dec6611dcc2ae6e
Brian <brianru@live.unc.edu>
2019-11-11 13:55:01 -0500

    updated wipe board and updateboard

--- src/ourTicTacToe/Board3D.java
+++ src/ourTicTacToe/Board3D.java
@@ -4,12 +4,16 @@ public class Board3D {
 	private Point[][][] board = new Point[4][4][4];
 	public Board3D(){
 		
+		//PLAYER ONE USES X'ES, PLAYER TWO USES O'S
+		
+		//each point should have numGamesPlayed property and numGamesWon property.  these will be used for reward function
+		
 		//populate board with values
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
 				for (int k=0; k<4; k++) {
 					board[i][j][k] = new Point();
-	
+					
 				}
 			}
 		}
@@ -18,10 +22,13 @@ public class Board3D {
 	
 	//update board is passed a boolean indicating which player made the move and three ints giving position of changed point
 	public void updateBoard(boolean isP1, int x, int y, int z) {  
-		if (board[x][y][z].value == 0) {   //if the square is blank
-			
+		if (board[x][y][z].value == null) {   //if the square is blank
+			if (isP1) {
+				board[x][y][z].value = "X";
+			} else {
+				board[x][y][z].value = "O";
+			}
 		}
-		//board[x][y][z].
 
 	}
 	
@@ -29,11 +36,11 @@ public class Board3D {
 		return this.board;
 	}
 	
-	public Point[] getXes() {
+	public Point[] getXes() { // if p1 wins, used by solver to update numGamesPlayed and numGamesWon for each individual point
 		
 	}
 	
-	public Point[] getYs() {
+	public Point[] getOs() {  // if p2 wins, used by solver to update numGamesPlayed and numGamesWon for each individual point
 		
 	}
 	
@@ -42,8 +49,8 @@ public class Board3D {
 		for (int i=0; i<4; i++) {
 			for (int j=0; j<4; j++) {
 				for (int k=0; k<4; k++) {
-					board[i][j][k].value = 0; //adjust so that points property of currentvalue is correct
-	
+					board[i][j][k].value = null; //adjust so that points property of currentvalue is correct
+					
 				}
 			}
 		}
