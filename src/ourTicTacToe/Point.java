fc5d8dd9555e0320c5a6fa62bbcd0d37b134d649
Angello Polo <apoloes@live.unc.edu>
2019-11-11 13:46:02 -0500

    Point changed

--- src/ourTicTacToe/Point.java
+++ src/ourTicTacToe/Point.java
@@ -1,14 +1,42 @@
 package ourTicTacToe;
 
 public class Point {
-
-	public int thing = 0;
-	public int other = 9;
-	public int also = 6;
+	int state; //
+	float util_val;
+	int num_wins;
+	int num_games;
+	int placeholder;
 	
-	public int onlyonBrianBranch = 50;
+	public Point(int s){
+		state = s;
+		util_val = 0;
+		num_wins = 0;
+		num_games = 0;
+		
+	}
 	
+	//Change the state of the Point (-1: O; 0: blank; 1: X)
+	public void changeState(int newState){
+		state = newState;
+	}
 	
+	//Increase or decrease the value depending on game outcome
+	public int updateValue(int outcome){
+		if (outcome == 1){	//Part of the winning solution
+			util_val += 0.05;
+		} else if (outcome == -1){ //Not part of the winning solution
+			util_val -= 0.05; 
+		}
+		return util_val;
+	}	
 	
-	public boolean anotherone;
+	public int getNumWins(){
+		
+	}
+	public int getNumGames(){
+		
+	}
+	public int getUtilValue(){
+		return num_wins/num_games;
+	}
 }
